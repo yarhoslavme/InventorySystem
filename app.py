5f8303853c0081ad3da5ba99872855aa70bf2bec
Yarhoslav ME <yarhoslavme@gmail.com>
2020-12-20 00:15:39 -0500

    Added dynamic menus to the Layaout

--- app.py
+++ app.py
@@ -8,14 +8,24 @@ app = Flask(__name__)
 init_db()
 app.config.update(SECRET_KEY="GRUPO3-R")
 
+rutas_admin = [["/", "Inicio"], ["/logout", "Salir"]]
+rutas_no_user = [["/login", "Login"]]
+
 
 @app.route("/")
 def index():
+    if "rol" in session:
+        session["rutas"] = rutas_admin
+    else:
+        session["rutas"] = rutas_no_user
     return render_template("index.html")
 
 
 @app.route("/login", methods=("GET", "POST"))
 def login():
+    if "rol" in session:
+        return redirect(url_for("index"))
+
     form = formulario_login()
 
     if form.validate_on_submit():
@@ -24,8 +34,10 @@ def login():
         usuariodb = usuario_by_username(username)
         if (usuariodb is not None) and (security.check_password_hash(usuariodb[1], password)):
             session["rol"] = usuariodb[3]
+            session["rutas"] = rutas_admin
             return redirect(url_for("index"))
         else:
+            session["rutas"] = rutas_no_user
             flash("Usuario o contraseña incorrectos")
 
     return render_template("login.html", form=form)
@@ -35,6 +47,7 @@ def login():
 def logout():
     if "rol" in session:
         session.pop("rol", None)
+        session["rutas"] = rutas_no_user
         return redirect(url_for("index"))
     return redirect(url_for("login"))
 
